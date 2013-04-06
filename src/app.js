c45ace446d5c8743dbcc50e1d90becb42fff9021
Mikael Brockman <mikael.brockman@gmail.com>
2013-04-06 17:47:52 +0200

    Start work on event store and registration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 var express = require('express')
   , passport = require('passport')
   , crypto = require('crypto')
-  , RedditStrategy = require('passport-reddit').Strategy;
+  , RedditStrategy = require('passport-reddit').Strategy
+  , _ = require('underscore');
 
 passport.use(
   new RedditStrategy({
@@ -10,11 +11,12 @@ passport.use(
     callbackURL: "http://goula.sh/auth/reddit/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function() {
-      console.log("New token! " + accessToken);
-      return done(null, profile);
-    })
-  }));
+    console.log("reddit id " + profile.id);
+    UserRegistry.findOrCreateRedditUser(profile.id, profile.name, function(user) {
+      done(null, user);
+    });
+  }
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -37,6 +39,74 @@ app.configure(function() {
   app.use(app.router);
 });
 
+var EventStore = {
+  listeners: [],
+  events: []
+};
+
+EventStore.push = function(event) {
+  this.events.push(event);
+};
+
+EventStore.registerListener = function(listener, eventTypes) {
+  function makeFilteringListener() {
+    return function(event) {
+      if (_.contains(eventTypes, event.eventType)) {
+        listener(event);
+      }
+    };
+  }
+
+  if (eventTypes !== undefined) {
+    this.registerListener(makeFilteringListener());
+  } else {
+    this.listeners.push(listener);
+  }
+};
+
+var UserRegistry = {
+  users: {}
+};
+
+UserRegistry.findOrCreateRedditUser = function(redditId, name, callback) {
+  function isTheOne(user) {
+    return user.credentials.redditId === redditId;
+  }
+
+  var user = _.find(_.values(this.users), isTheOne)
+
+  if (user) {
+    callback(user);
+  } else {
+    var aggregateId = _.uniqueId('user');
+    var newUser = {
+      aggregateId: aggregateId,
+      userName: name,
+      credentials: {
+        redditId: redditId
+      }
+    };
+
+    EventStore.push({
+      eventType: 'user-registered',
+      aggregateId: aggregateId,
+      payload: newUser
+    });
+
+    this.users[aggregateId] = newUser;
+
+    callback(newUser);
+  }
+};
+
+EventStore.registerListener(function(event) {
+  UserRegistry.users[event.aggregateId] = {
+    aggregateId: event.aggregateId,
+    userName: event.payload.userName,
+    credentials: event.payload.credentials
+  };
+}, ['user-registered']);
+
 app.get('/', function(req, res) {
   if (req.user) {
     res.redirect('/start');
@@ -55,17 +125,21 @@ app.get('/auth/reddit', function(req, res, next) {
 app.get('/auth/reddit/callback', function(req, res, next) {
   if (req.query.state === req.session.state) {
     passport.authenticate('reddit', {
-      successRedirect: '/start',
-      failureRedirect: '/login'
+      successRedirect: '/auth/done',
+      failureRedirect: '/'
     })(req, res, next);
   } else {
     next(new Error(403));
   }
 });
 
+app.get('/auth/done', function(req, res) {
+  res.redirect('/start');
+});
+
 app.get('/start', function(req, res) {
-  console.log(req.user);
-  res.send('Welcome ' + req.user.name + '!');
+  console.log('User: ' + req.user);
+  res.send('Welcome ' + JSON.stringify(req.user) + '!');
 });
 
 exports.goulash = {
